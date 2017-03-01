45a09a42b13a0bb34ca1be75c739a73bc375fe94
dodjitche <aureltche@gmail.com>
2017-03-01 11:03:28 -0500

    Reviewing python

---
+++ project.py
@@ -0,0 +1,13 @@
+'''
+greeting = 'Hello World'
+
+print "greeting[0:6] ", greeting[0:6]
+
+
+'''
+
+names = ['Lyle PUENTE', 'Tyler Joseph', 'Josh Dun']
+print names [0]
+
+
+dic = {} kwlckchbckbwclhbwcliwhbc
